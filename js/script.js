6c0ef16d033d83b5df03fc002763a3dcb2ea9e35
reagan2020 <reaganteflon@.com>
2021-06-20 20:12:39 +0300

    add js file

---
+++ js/script.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+      $("#design-words").toggle();
+      $("#design-image").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#development").click(function(){
+      $("#development-words").toggle();
+      $("#development-image").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#product-management").click(function(){
+      $("#product-management-words").toggle();
+      $("#product-management-image").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#work4").mouseover(function(){
+      $("#overlay4").show();
+    }).mouseout(function(){
+      $("#overlay4").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work3").mouseover(function(){
+      $("#overlay3").show();
+    }).mouseout(function(){
+      $("#overlay3").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work2").mouseover(function(){
+      $("#overlay2").show();
+    }).mouseout(function(){
+      $("#overlay2").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work1").mouseover(function(){
+      $("#overlay1").show();
+    }).mouseout(function(){
+      $("#overlay1").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work5").mouseover(function(){
+      $("#overlay5").show();
+    }).mouseout(function(){
+      $("#overlay5").hide();
+    });
+  });  
+  $(document).ready(function(){
+    $("#work6").mouseover(function(){
+      $("#overlay6").show();
+    }).mouseout(function(){
+      $("#overlay6").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work7").mouseover(function(){
+      $("#overlay7").show();
+    }).mouseout(function(){
+      $("#overlay7").hide();
+    });
+  }); 
+  $(document).ready(function(){
+    $("#work8").mouseover(function(){
+      $("#overlay8").show();
+    }).mouseout(function(){
+      $("#overlay8").hide();
+    });
+  });     
+  $(document).ready(function(){
+    $("#submit").click(function(){
+        let name = $("#name").val();
+        let email = $("#email").val();
+        let message = $("#comment").val();
+        if(name,email, message == ''){
+         alert("Fill all the field entries to make a complete subscription")
+        }
+        else {
+            alert(`Greetings ${name}! You have just joined the best Mailing list ever! Be sure to keep an eye on ${email} for the best offers from us.`);
+        }
+    })
+})  
